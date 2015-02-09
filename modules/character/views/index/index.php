06dd243b077e1ba4525760d8a83f9ec3902b46f5
Sergey <sergey.xz01d@gmail.com>
2015-02-09 23:18:36 +0300

    Fix. Clear character module index page.

--- modules/character/views/index/index.php
+++ modules/character/views/index/index.php
@@ -1,11 +1,27 @@
-<?php ?>
-
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h1 class="panel-title"><?= $mCharacter->characterName; ?></h1>
+        <div class="pull-right">ID: <?= $mCharacter->characterID; ?></div>
+        <h1 class="panel-title">Character :: <?= $mCharacter->characterName; ?></h1>
     </div>
 
-    <div class="panel-body"></div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-sm-3 text-center">
+                <img class="img-thumbnail" src="https://image.eveonline.com/Corporation/<?= $mCharacter->corporationID; ?>_128.png">
+                <h3><?= $mCharacter->corporationName; ?></h3>
+            </div>
 
-    <div class="panel-footer"></div>
+            <div class="col-sm-6 text-center">
+                <img class="img-thumbnail" src="https://image.eveonline.com/Character/<?= $mCharacter->characterID; ?>_256.jpg">
+                <h3><?= $mCharacter->characterName; ?></h3>
+            </div>
+
+            <div class="col-sm-3 text-center">
+                <?php if ($mCharacter->allianceID): ?>
+                    <img class="img-thumbnail" src="https://image.eveonline.com/Alliance/<?= $mCharacter->allianceID; ?>_128.png">
+                    <h3><?= $mCharacter->allianceName; ?></h3>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </div>
