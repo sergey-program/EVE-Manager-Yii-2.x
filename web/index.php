ff56b129d940f5441ae07775a5e3731be8c82c3c
sergey <sergey>
2017-01-10 04:35:29 +0300

    clear

--- web/index.php
+++ web/index.php
@@ -8,13 +8,21 @@ define('FILE_PATH_ROOT', FILE_PATH_ROOT_WEB . '..' . DS);
 define('FILE_PATH_ROOT_UPLOAD', FILE_PATH_ROOT_WEB . DS . 'uploads' . DS);
 define('FILE_PATH_VENDOR', FILE_PATH_ROOT . 'vendor' . DS);
 
-defined('YII_ENV') or define('YII_ENV', file_exists(FILE_PATH_ROOT . '.prod') ? 'prod' : 'dev');
-defined('YII_DEBUG') or define('YII_DEBUG', YII_ENV === 'dev');
+// default (local) env is 'test'
+$env = 'test';
+$env = file_exists(FILE_PATH_ROOT . '.prod') ? 'prod' : $env;
+$env = file_exists(FILE_PATH_ROOT . '.dev') ? 'dev' : $env;
+defined('YII_ENV') or define('YII_ENV', $env);
+
+// on dev and test env enable debug
+$debug = file_exists(FILE_PATH_ROOT . '.debug') ? true : false;
+$debug = (YII_ENV == 'dev' || YII_ENV == 'test') ? true : $debug;
+defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 
 define('FILE_PATH_CONFIG', FILE_PATH_ROOT . 'config' . DS);
 define('FILE_PATH_CONFIG_ENV', FILE_PATH_CONFIG . YII_ENV . DS);
 
-if (YII_ENV_DEV) {
+if ($env != 'prod' || $debug) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 }
@@ -22,7 +30,7 @@ if (YII_ENV_DEV) {
 require(FILE_PATH_VENDOR . 'autoload.php');
 require(FILE_PATH_VENDOR . 'yiisoft' . DS . 'yii2' . DS . 'Yii.php');
 
-$config = require(FILE_PATH_CONFIG_ENV . 'main_site.php');
+$config = require(FILE_PATH_CONFIG_ENV . 'main.php');
 
 $application = new Application($config);
 $application->run();
