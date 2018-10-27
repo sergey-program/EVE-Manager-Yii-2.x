c0c2be747d9fed0b844071f1b81f91ca97a5a6ec
sergey <sergey.xz01d@gmail.com>
2018-10-27 23:57:36 +0300

    Blueprint. Load product and settings from model.

--- components/items/TraitBlueprint.php
+++ components/items/TraitBlueprint.php
@@ -2,6 +2,7 @@
 
 namespace app\components\items;
 
+use app\models\BlueprintSettings;
 use app\models\dump\IndustryActivityProducts;
 
 /**
@@ -11,8 +12,12 @@ use app\models\dump\IndustryActivityProducts;
  */
 trait TraitBlueprint
 {
+    /** @var BlueprintSettings|null $settings */
+    public $settings;
     /** @var Item|null|false $blueprint */
-    protected $blueprint = null;
+    protected $blueprint;
+    /** @var Item|null|false $product */
+    protected $product;
 
     /**
      * @return $this
@@ -25,7 +30,43 @@ trait TraitBlueprint
             $this->blueprint = $iam ? new Item(['invType' => $iam->invType]) : false;
         }
 
+        if ($this->isBlueprint()) {
+            $settings = BlueprintSettings::findOne([
+                'userID' => \Yii::$app->user->id,
+                'typeID' => $this->typeID
+            ]);
+
+            if (!$settings) {
+                $settings = new BlueprintSettings([
+                    'userID' => \Yii::$app->user->id,
+                    'typeID' => $this->typeID
+                ]);
+                $settings->save();
+            }
+
+            $this->settings = $settings;
+        }
+
         return $this;
+    }
+
+    public function getProduct()
+    {
+        if (is_null($this->product)) {
+            $filter = ['typeID' => $this->typeID, 'activityID' => 1];
+            $iam = IndustryActivityProducts::find()->where($filter)->cache(60 * 60 * 24)->one();
+            $this->product = $iam ? new Item(['invType' => $iam->productType]) : false;
+        }
+
+        return $this->product;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBlueprint()
+    {
+        return is_int(strpos($this->typeName, 'Blueprint'));
     }
 
     /**
@@ -34,5 +75,29 @@ trait TraitBlueprint
     public function hasBlueprint()
     {
         return $this->blueprint ? true : false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasSettings()
+    {
+        return $this->settings ? true : false;
+    }
+
+    /**
+     * @return Item|false|null
+     */
+    public function getBlueprint()
+    {
+        return $this->blueprint;
+    }
+
+    /**
+     * @return BlueprintSettings|null
+     */
+    public function getSettings()
+    {
+        return $this->settings;
     }
 }
--- components/items/~TraitManufacture.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace app\components\items;
-
-trait TraitManufacture
-{
-
-}
