9983680fc9fdd3f235d1727ba618407534f54557
sergey <sergey.xz01d@gmail.com>
2018-10-29 16:04:28 +0400

    Base working trait blueprint.

--- components/items/TraitBlueprint.php
+++ components/items/TraitBlueprint.php
@@ -13,111 +13,126 @@ use app\models\dump\IndustryActivityProducts;
  */
 trait TraitBlueprint
 {
-    /** @var Item|null|false $child */
+    /** @var Item|null|false $blueprint */
     protected $blueprint;
-    /** @var BlueprintSettings|null $settings */
-    protected $settings;
-    /** @var Item|null|false $product */
-    protected $product;
-    /** @var Item[]|null|false $materials */
-    protected $materials;
+    /** @var BlueprintSettings|null $blueprintSettings */
+    protected $blueprintSettings;
+    /** @var Item|null|false $blueprintProduct */
+    protected $blueprintProduct;
+    /** @var Item[]|null|false $blueprintMaterials */
+    protected $blueprintMaterials;
+    /** @var int $blueprintRuns */
+    protected $blueprintRuns = 1;
 
     /**
-     * Return blueprint that uses to manufacture current item.
+     * @param int $runs
      *
+     * @return $this
+     */
+    public function setRuns($runs)
+    {
+        $this->blueprintRuns = $runs;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRuns()
+    {
+        return $this->blueprintRuns;
+    }
+
+    /**
+     * @param int|null $quantity
+     *
+     * @return null
+     */
+    public function calculateRuns($quantity = null)
+    {
+        // @todo bpo can do 100\40\1 per run, recalculate it
+        return is_null($quantity) ? $this->getQuantity() : $quantity;
+    }
+
+    /**
      * @return Item|false|null
      */
     public function getBlueprint()
     {
-        if (!$this->isBlueprint()) { // load only if item NOT blueprint
-            if (is_null($this->blueprint)) {
-                $filter = ['productTypeID' => $this->typeID, 'activityID' => 1];
-                $product = IndustryActivityProducts::find()->where($filter)->cache(60 * 60 * 24)->one();
-                $this->blueprint = $product ? new Item(['invType' => $product->invType]) : false;
-            }
+        if (is_null($this->blueprint)) {
+            $filter = ['productTypeID' => $this->typeID, 'activityID' => 1];
+            $product = IndustryActivityProducts::find()->where($filter)->cache(60 * 60 * 24)->one();
+            $this->blueprint = $product ? new Item(['invType' => $product->invType, 'runs' => $this->calculateRuns()]) : false;
         }
 
         return $this->blueprint;
     }
 
     /**
-     * Return item that can be manufactured by this item (bpo).
+     * What this bpo produces.
      *
      * @return Item|bool|false|null
      */
     public function getProduct()
     {
-        if ($this->isBlueprint()) { // only bpo has materials
-            if (is_null($this->product)) {
-                $filter = ['typeID' => $this->typeID, 'activityID' => 1];
-                $product = IndustryActivityProducts::find()->where($filter)->cache(60 * 60 * 24)->one();
-                $this->product = $product ? new Item(['invType' => $product->productType]) : false;
-            }
+        if (is_null($this->blueprintProduct)) {
+            $filter = ['typeID' => $this->typeID, 'activityID' => 1];
+            $product = IndustryActivityProducts::find()->where($filter)->cache(60 * 60 * 24)->one();
+            $this->blueprintProduct = $product ? new Item(['invType' => $product->productType]) : false;
         }
 
-        return $this->product;
-    }
-
-    /**
-     * Return raw list of materials.
-     *
-     * @return IndustryActivityMaterials[]|array|null
-     */
-    public function getBaseMaterials()
-    {
-        if ($this->isBlueprint()) { // only bpo has materials
-            return IndustryActivityMaterials::find()->where(['typeID' => $this->typeID, 'activityID' => 1])->all();
-        }
-
-        return null;
+        return $this->blueprintProduct;
     }
 
     /**
      * Return calculated list of items for manufacture.
      *
-     * @return bool|Item[]
+     * @return Item[]|bool
      */
     public function getMaterials()
     {
-        if ($this->isBlueprint()) { // only bpo has materials
-            if (is_null($this->materials)) {
-                $materials = $this->getBaseMaterials();
+        if (is_null($this->blueprintMaterials)) {
+            $materials = IndustryActivityMaterials::find()->where(['typeID' => $this->typeID, 'activityID' => 1])->cache(60 * 60 * 24)->all();
 
-                if (empty($materials)) {
-                    $this->materials = false;
-                } else {
-                    foreach ($materials as $material) {
-                        $this->materials[] = $this->createMaterialItem($material); // before apply count me
-                    }
+            if (empty($materials)) {
+                $this->blueprintMaterials = false;
+            } else {
+                foreach ($materials as $material) {
+                    $item = $material->materialInvType->getItem();
+                    $item->setQuantity($material->quantity);
+                    $item->setParentBlueprint($this); // remember parent bpo to apply me\te
+
+                    $this->blueprintMaterials[] = $item;
                 }
             }
         }
 
-        return $this->materials;
+        return $this->blueprintMaterials;
     }
 
     /**
-     * Create new child item (for manufacture), calculate real quantity using me (settings).
+     * @param int $rawQuantity
      *
-     * @param IndustryActivityMaterials $material
-     *
-     * @return Item
+     * @return float|int
      */
-    private function createMaterialItem(IndustryActivityMaterials $material)
+    public function calculateQuantity($rawQuantity)
     {
-        // calculate users quantity
-
         $me = 1 - ($this->getSettings()->me / 100);
         $meRig = 1 - ($this->getSettings()->meRig / 100);
         $meHull = 1 - ($this->getSettings()->meHull / 100);
 
-        // @todo calculate settings runPrice and te
+        return ceil($rawQuantity * $me * $meRig * $meHull); // * $this->getRuns();
+    }
 
-        $item = $material->materialInvType->getItem();
-        $item->setQuantity(ceil($material->quantity * $me * $meRig * $meHull));
-        $item->setParentBlueprint($this); // remember parent bpo
-
-        return $item;
+    /**
+     * This item has bpo settings (me\te\runPrice).
+     *
+     * @return bool
+     */
+    public function hasSettings()
+    {
+        return $this->getSettings() ? true : false;
     }
 
     /**
@@ -131,42 +146,20 @@ trait TraitBlueprint
     }
 
     /**
-     * Check this item can be manufactured by bpo.
-     *
-     * @return bool
-     */
-    public function hasBlueprint()
-    {
-        return $this->getBlueprint() ? true : false;
-    }
-
-    /**
-     * This item has bpo settings (me\te\runPrice).
-     *
-     * @return bool
-     */
-    public function hasSettings()
-    {
-        return $this->settings ? true : false;
-    }
-
-    /**
      * @return BlueprintSettings|null
      */
     public function getSettings()
     {
-        if ($this->isBlueprint()) { // only bpo can own settings
-            $filter = ['userID' => \Yii::$app->user->id, 'typeID' => $this->typeID];
-            $settings = BlueprintSettings::findOne($filter);
+        $filter = ['userID' => \Yii::$app->user->id, 'typeID' => $this->typeID];
+        $settings = BlueprintSettings::findOne($filter);
 
-            if (!$settings) { // create empty entry to have zero defaults
-                $settings = new BlueprintSettings($filter);
-                $settings->save();
-            }
-
-            $this->settings = $settings;
+        if (!$settings) { // create empty entry to have zero defaults
+            $settings = new BlueprintSettings($filter);
+            $settings->save();
         }
 
-        return $this->settings;
+        $this->blueprintSettings = $settings;
+
+        return $this->blueprintSettings;
     }
 }
